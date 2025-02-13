08f2473d440fbb0c513c6b1ec5e91d9df167fa64
Taslim Ansari <1993taslim@gmail.com>
2025-02-14 03:10:29 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -5,12 +5,34 @@ const port = 3000;
 // Serve static files
 app.use(express.static('public'));
 
+// Updated routes based on new directory structure
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/about.html');
 });
 
-app.get('/skills', (req, res) => {
-  res.sendFile(__dirname + '/public/skills.html');
+
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/public/contact.html');
+});
+
+app.get('/portfolio', (req, res) => {
+  res.sendFile(__dirname + '/public/portfolio.html');
+});
+
+app.get('/portfolio-details', (req, res) => {
+  res.sendFile(__dirname + '/public/portfolio-details.html');
+});
+
+app.get('/resume', (req, res) => {
+  res.sendFile(__dirname + '/public/resume.html');
+});
+
+app.get('/services', (req, res) => {
+  res.sendFile(__dirname + '/public/services.html');
+});
+
+app.get('/starter-page', (req, res) => {
+  res.sendFile(__dirname + '/public/starter-page.html');
 });
 
 app.listen(port, () => {
